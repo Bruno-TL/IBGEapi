d511bf53d04c0e8ed0b236e71d7fa91e638d57e7
Thiago Bruno <tbgt35@hotmail.com>
2022-09-28 21:05:58 -0300

    Finalizando a lógica e os estilos

--- eventos.js
+++ eventos.js
@@ -1,11 +1,95 @@
 fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
     .then( resposta => resposta.json())
     .then( regioes => {
-        regioes.map( (a) => {
+        regioes.map( a => {
         document.getElementById('regiao').innerHTML += 
         `
-        <option>${a.nome}</option>
-        `});
-    })
+        <option value="${a.id}" id="${a.id}">${a.nome}</option>`
+        });
+        
+    });
 
+const criarListaEstado = (id) => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`)
+        .then( resposta => resposta.json())
+        .then( res => {
+
+            let nome = document.getElementById(`${id}`).innerHTML;
+            aparecerRegiao(nome)
+
+            res.map( a => {
+            document.getElementById('estado').innerHTML += 
+            `
+            <option value="${a.id}" id="${a.id}">${a.nome}</option>`
+            });
+            
+            
+        });
+};
+
+const criarListaCidade = (id) => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/distritos`)
+        .then( resposta => resposta.json())
+        .then( res => { 
+            let nome = document.getElementById(`${id}`).innerHTML;
+            aparecerEstado(nome)
+
+            res.map( a => {
+                document.getElementById('cidade').innerHTML +=
+                `
+                <option value="${a.id}" id="${a.id}">${a.nome}</option>
+                `
+            });
+        });
+};
+
+const pegandoValueCidade = (id) => {
+    let nome = document.getElementById(`${id}`).innerHTML;
+    aparecerCidade(nome)
+}
+
+
+//Funções para fazer aparecer o quadrodos informando a Região, estado e cidade.
+
+const aparecerRegiao = (nome) => {
+    let n = document.getElementById('regiaoTexto');
+        n.classList.add("aparecer")
+        n.classList.add("animate__animated");
+        n.classList.add("animate__backInDown");
+        n.classList.add("animate__delay-0.5s");
+
+        document.getElementById('regiaoTexto').innerHTML = 
+        `
+        <h2>Sua Região</h2>
+        <p class="fs-2">${nome}</p>
+        `
+}
+
+const aparecerEstado = (nome) => {
+    let n = document.getElementById('estadoTexto');
+        n.classList.add("aparecer");
+        n.classList.add("animate__animated");
+        n.classList.add("animate__backInDown");
+        n.classList.add("animate__delay-0.5s");
+
+        document.getElementById('estadoTexto').innerHTML = 
+        `
+        <h2>Seu Estado</h2>
+        <p class="fs-2">${nome}</p>
+        `
+}
+
+const aparecerCidade = (nome) => {
+    let n = document.getElementById('cidadeTexto');
+        n.classList.add("aparecer");
+        n.classList.add("animate__animated");
+        n.classList.add("animate__backInDown");
+        n.classList.add("animate__delay-0.5s");
+
+        document.getElementById('cidadeTexto').innerHTML = 
+        `
+        <h2>Sua Cidade</h2>
+        <p class="fs-2">${nome}</p>
+        `
+}
 
